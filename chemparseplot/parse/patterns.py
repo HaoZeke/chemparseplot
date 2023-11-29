84094870781bb348851437c040c8cda8f30b9791
Rohit Goswami <rog32@hi.is>
2023-11-29 09:28:39 +0000

    MAINT: Add more documentation on the 2col pattern

--- chemparseplot/parse/patterns.py
+++ chemparseplot/parse/patterns.py
@@ -3,4 +3,16 @@
 # SPDX-License-Identifier: MIT
 
 # https://regex101.com/r/jHAG2T/1
-TWO_COL_NUM = r"\s(?P<twocolnum>(?:\s+\d+\.\d+\s+-?\d+\.\d+)+)"
+# DIGIT pattern for a floating-point number, possibly negative
+DIGIT = r"-?\d+\.\d+"
+TWO_COL_NUM = (
+    r"\s*"  # Optional whitespace
+    r"(?P<twocolnum>"  # Named group 'twocolnum' starts
+    r"(?:"
+    r"\s*"  # Optional whitespace
+    + DIGIT
+    + r"\s+"  # whitespace characters between numbers
+    + DIGIT
+    + r")+)"  # The non-capturing group ends;
+    # '+' allows for one or more occurrences of the pattern
+)
